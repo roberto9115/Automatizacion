1d3c3ab8273e07091bdbcd7341c8bd13a32b2b17
unknown <robertomaxariegos@gmail.com>
2023-02-27 12:26:09 -0600

    cambio 4

--- src/main/java/HsbcMXPage/HomePage/HomePageHSBC.java
+++ src/main/java/HsbcMXPage/HomePage/HomePageHSBC.java
@@ -35,9 +35,9 @@ public class HomePageHSBC extends CommonMethods {
 
 
     public void menuCreditoNomina() throws InterruptedException {
-//        Thread.sleep(30000);
-        clickElement(menuCredito);
         Thread.sleep(30000);
+        clickElement(menuCredito);
+        Thread.sleep(5000);
         clickElement(menuCreditoNomina);
         //functionMouseOver(cuentaDebito, cambiaNomina);
     }
